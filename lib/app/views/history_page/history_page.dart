46aa9fda8e1d85f8ff6b1b64425a68186910e65b
Felipe Mazieri <44025173+mazieri@users.noreply.github.com>
2022-03-19 02:00:46 -0300

    add dependency injection + controller + reaction

--- lib/app/views/history_page/history_page.dart
+++ lib/app/views/history_page/history_page.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:get_it/get_it.dart';
+
+import '../../controllers/controllers_exports.dart';
 
 class HistoryPage extends StatelessWidget {
   const HistoryPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final historyController = GetIt.I.get<LicenseController>();
+
     return SafeArea(
-      child: ListView.separated(
-        itemCount: 20,
-        separatorBuilder: (context, index) => Divider(
-          color: Colors.black,
-        ),
-        itemBuilder: (context, index) => ListTile(
-          title: Text('Carro $index'),
-        ),
+      child: Observer(
+        builder: (_) {
+          return ListView.separated(
+            itemCount: historyController.historyParkingLotsList.length,
+            separatorBuilder: (context, index) => const Divider(),
+            itemBuilder: (context, index) => ListTile(
+              title: Text(
+                  "O Veículo de placa ${historyController.historyParkingLotsList[index].toUpperCase()}, entrou"),
+            ),
+          );
+        },
       ),
     );
   }
