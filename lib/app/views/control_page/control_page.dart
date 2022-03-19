bbf1c5651c2661b56ab9f62c502fa0aa4bb2c89e
Felipe Mazieri <44025173+mazieri@users.noreply.github.com>
2022-03-19 02:00:37 -0300

    add dependency injection + controller + reaction

--- lib/app/views/control_page/control_page.dart
+++ lib/app/views/control_page/control_page.dart
@@ -1,67 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:get_it/get_it.dart';
+
+import '../../controllers/controllers_exports.dart';
 
 class ControlPage extends StatelessWidget {
   const ControlPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final parkingController = GetIt.I.get<LicenseController>();
+    TextEditingController textController = TextEditingController();
+
     return SafeArea(
       child: Column(
         children: [
           Expanded(
             child: Container(
-              padding: const EdgeInsets.all(10),
-              color: Colors.green,
-              child: GridView.count(
-                scrollDirection: Axis.horizontal,
-                crossAxisCount: 2,
-                mainAxisSpacing: 10,
-                crossAxisSpacing: 10,
-                semanticChildCount: 20,
-                children: List.generate(20, (index) {
-                  return Container(
-                    padding: EdgeInsets.all(10),
-                    width: 50,
-                    height: 50,
-                    color: Colors.black,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text("Placa do Carro $index"),
-                        Container(
-                          width: 70,
-                          height: 70,
-                          color: Colors.orange,
+                padding: const EdgeInsets.all(10),
+                color: Colors.green,
+                child: Observer(builder: (_) {
+                  return GridView.count(
+                    scrollDirection: Axis.horizontal,
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 10,
+                    crossAxisSpacing: 10,
+                    semanticChildCount: 20,
+                    children: List.generate(
+                        parkingController.parkingLotsList.length, (index) {
+                      return SizedBox(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              parkingController.parkingLotsList[index]
+                                  .toString(),
+                            ),
+                            Expanded(
+                              child: Image.asset(
+                                "assets/car_top_shape.png",
+                                fit: BoxFit.contain,
+                              ),
+                            ),
+                            ElevatedButton(
+                              onPressed: () =>
+                                  parkingController.removeParkingLot(
+                                parkingController.parkingLotsList[index],
+                              ),
+                              child: const Center(
+                                child: Text("Saída"),
+                              ),
+                            ),
+                          ],
                         ),
-                        ElevatedButton(
-                          onPressed: () {},
-                          child: Center(
-                            child: Text("Saída"),
-                          ),
-                        ),
-                      ],
-                    ),
+                      );
+                    }),
                   );
-                }),
-              ),
-            ),
+                })),
           ),
           Expanded(
             child: Container(
-              padding: EdgeInsets.all(10),
+              padding: const EdgeInsets.all(10),
               color: Colors.red,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
+                  Observer(builder: (_) {
+                    return Text("Vagas Utilizadas: " +
+                        parkingController.parkingLotsNumber.toString());
+                  }),
                   TextField(
-                    decoration: InputDecoration(
+                    controller: textController,
+                    onChanged: parkingController.setLicenseValue,
+                    decoration: const InputDecoration(
                       labelText: "Placa do Veículo",
                       border: OutlineInputBorder(),
                     ),
                   ),
                   ElevatedButton(
-                    onPressed: () {},
-                    child: Center(
+                    onPressed: () {
+                      parkingController.useParkingLot();
+                      parkingController.addToHistoryParkingLot();
+                      textController.clear();
+                    },
+                    child: const Center(
                       child: Text("Marcar Entrada do Veículo"),
                     ),
                   ),
